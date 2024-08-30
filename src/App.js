84db25f8489b71f3f41dd5f1c675e315ab25e965
kimsh142536 <kimsh142536@gmail.com>
2024-08-30 17:19:29 +0900

    [Feat] Close Panel when click Link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import PanelControl from 'component/common/PanelControl';
 import { AuthContext } from 'component/auth/AuthContext';
 import './App.css';
 import Footer from 'component/common/Footer';
-
 function App() {
   const [isPanelOpen, setIsPanelOpen] = useState(false);
   const [user, setUser] = useState(null); // 사용자 상태 관리
@@ -15,19 +14,23 @@ function App() {
     setIsPanelOpen(!isPanelOpen);
   };
 
+  const closePanel = () => {
+    setIsPanelOpen(false);
+  };
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <NavBar togglePanel={togglePanel} />
       <div className="App">
         <div className="main">
-          {isPanelOpen && <PanelControl />} {/* 사이드바 표시 여부 */}
+          {isPanelOpen && <PanelControl closePanel={closePanel} />} {/* 사이드바 표시 여부 */}
           <div className="content">
             {/* 현재 경로에 맞는 자식 컴포넌트를 렌더링 */}
             <Outlet />
           </div>
         </div>
       </div>
-      <Footer></Footer>
+      <Footer />
     </AuthContext.Provider>
   );
 }
